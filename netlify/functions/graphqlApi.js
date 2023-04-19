bd2226c05b83d24b5a64a195fae65548c63c98e0
CrimsonCoder42 <53884312+CrimsonCoder42@users.noreply.github.com>
2023-04-19 10:39:32 -0400

    comment update

--- netlify/functions/graphqlApi.js
+++ netlify/functions/graphqlApi.js
@@ -1,37 +1,49 @@
-const fetch = require('node-fetch');
+// Import fetch from node-fetch
+const fetch = require("node-fetch");
 
+// Define the handler function for the Lambda
 const handler = async function (event, context) {
-    const apiUrl = 'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/photosapp-zxrdq/graphql'    
-    let graphqlQuery={};
-
+  // Set the API URL for the MongoDB Realm GraphQL endpoint
+  const apiUrl =
+    "https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/photosapp-zxrdq/graphql";
+  let graphqlQuery = {};
 
   try {
-    const response = await fetch(apiUrl,  {
+    // Send a POST request to the API URL
+    const response = await fetch(apiUrl, {
       method: "post",
-      headers: { 
-        Accept: 'application/json',
+      headers: {
+        Accept: "application/json",
         apiKey: `${process.env.MONGO_API_KEY}`,
-        },
-      body:JSON.stringify(graphqlQuery)
-    }) 
+      },
+      body: JSON.stringify(graphqlQuery),
+    });
+
+    // Check if the response is OK (status code 200-299)
     if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
+      // Return the status code and status text if not OK
+      return { statusCode: response.status, body: response.statusText };
     }
-    const data = await response.json() 
+
+    // Parse the response data
+    const data = await response.json();
+
+    // Return the parsed data with a 200 status code
     return {
       statusCode: 200,
       body: JSON.stringify({ data }),
-    }
+    };
   } catch (error) {
-    // output to netlify function log
-    console.log(error)
+    // Log the error to the Netlify function log
+    console.log(error);
+
+    // Return a 500 status code and the error message
     return {
       statusCode: 500,
-      // Could be a custom message or object i.e. JSON.stringify(err)
       body: JSON.stringify({ msg: error.message }),
-    }
+    };
   }
-}
-  
-module.exports = {handler}
+};
+
+// Export the handler function
+module.exports = { handler };
